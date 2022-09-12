701092195b045601a0675f8ae6f4279b764fec42
forrest <dlrkdgh111111@gmail.com>
2022-09-12 21:01:35 +0900

    서브테마 추가

---
+++ lib/thema/sub_thema.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+var mainThema = ThemeData(
+
+    textTheme: TextTheme(
+      // body에 사용되는 텍스트의 스타일
+        bodyText2: TextStyle(color: Color.fromRGBO(
+            67, 215, 25, 0.8117647058823529))
+    )
+
+);
